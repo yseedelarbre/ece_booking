7dedfb4f0b0192b008a1f1082643dc1b029b6e5f
Fabas <arthur.fabas@orange.fr>
2023-03-21 19:46:59 +0100

    Modification test Arthur

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("ece booking projet ");
+        System.out.println("Modifié par Arthur ");
+
     }
     int a ;
 }
