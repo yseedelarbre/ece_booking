70284c1e37113143b6f15e42d7c944e0a1d24689
SeverinJacquis <severinjacquis@gmail.com>
2023-03-22 16:28:00 +0100

    Update Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("ece booking projet ");
+        System.out.println("Modifié par Arthur ");
+        System.out.println("Modifié par Ysee ");
+        System.out.println("Modifié par Candice ");
+        System.out.println("AAAAAAAAAAAAAAAAAAA");
+        System.out.println("ca fonctionne");
+
+    }
+    //int a ;
+}
